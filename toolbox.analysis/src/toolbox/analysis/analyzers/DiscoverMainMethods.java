8573b68d44dfeaf44e725f2e82c965ad1a234a8c
Daniel Griffen <danielgriffen@ensoftcorp.com>
2015-10-09 16:08:24 -0500

    Update DiscoverMainMethods to Atlas 2.5.2

--- toolbox.analysis/src/toolbox/analysis/analyzers/DiscoverMainMethods.java
+++ toolbox.analysis/src/toolbox/analysis/analyzers/DiscoverMainMethods.java
@@ -1,8 +1,6 @@
 package toolbox.analysis.analyzers;
 
 import com.ensoftcorp.atlas.core.query.Q;
-import com.ensoftcorp.atlas.core.script.Common;
-import com.ensoftcorp.atlas.core.xcsg.XCSG;
 import com.ensoftcorp.open.toolbox.commons.analysis.Analyzer;
 
 public class DiscoverMainMethods extends Analyzer {
@@ -32,41 +30,8 @@ public class DiscoverMainMethods extends Analyzer {
 
 	@Override
 	protected Q evaluateEnvelope() {
-		// Step 1) select nodes from the index that are marked as public, static, methods
-		Q mainMethods = appContext.nodesTaggedWithAll(XCSG.publicVisibility, XCSG.ClassMethod);
-		
-		// Step 2) select nodes from the public static methods that are named "main"
-		mainMethods = mainMethods.selectNode(XCSG.name, "main");
-		
-		// Step 3) filter out methods that are not void return types
-		Q returnsEdges = context.edgesTaggedWithAny(XCSG.Returns).retainEdges();
-		Q voidMethods = returnsEdges.predecessors(Common.universe().nodesTaggedWithAny(XCSG.Void));
-		mainMethods = mainMethods.intersection(voidMethods);
-		
-		// Step 4) filter out methods that do not take exactly one parameter
-		Q paramEdges = appContext.edgesTaggedWithAny(XCSG.Parameter).retainEdges();
-		Q mainMethodParams = paramEdges.successors(mainMethods);
-		// methods with no parameters will not have a PARAM edge (and won't be reachable from parameters)
-		mainMethods = paramEdges.predecessors(mainMethodParams);
-		// methods with 2 or more parameters will have at least one parameter node with attribute 
-		// PARAMETER_INDEX == 1 (index 0 is the first parameter)
-		Q mainMethodSecondParams = mainMethodParams.selectNode(XCSG.parameterIndex, 1);
-		Q methodsWithTwoOrMoreParams = mainMethodSecondParams.predecessors(mainMethodSecondParams);
-		mainMethods = mainMethods.difference(methodsWithTwoOrMoreParams);
-
-		// TODO: update for XCSG
-//		// Step 5) filter out methods that do not take a one dimensional String array
-//		Q elementTypeEdges = context.edgesTaggedWithAny(Edge.ELEMENTTYPE).retainEdges();
-//		Q stringArraysTypes = elementTypeEdges.predecessors(Common.typeSelect("java.lang","String"));
-//		// array types have a DIMENSION attribute
-//		Q oneDimensionStringArrayType = stringArraysTypes.selectNode(Node.DIMENSION, 1);
-//		Q mainMethodFirstParams = CommonQueries.methodParameter(mainMethods, 0);
-//		Q typeOfEdges = context.edgesTaggedWithAny(Edge.TYPEOF).retainEdges();
-//		Q oneDimensionStringArrayParameters = typeOfEdges.predecessors(oneDimensionStringArrayType);
-//		Q validMainMethodFirstParams = mainMethodFirstParams.intersection(oneDimensionStringArrayParameters);
-//		mainMethods = paramEdges.predecessors(validMainMethodFirstParams);
-		
-		return mainMethods;
+		//TODO: Implement
+		return null;
 	}
 	
 }
